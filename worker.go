b4a9d0f21f04de3a00c26fc6ea7bcab606578286
Genetiro <skripkin.viktor.j@mail.com>
2022-09-13 19:48:17 +0300

    dz4_v2

--- worker.go
+++ worker.go
@@ -6,7 +6,8 @@ import (
 
 func worker(id int, jobs <-chan int, results chan<- int) {
 	for j := range jobs {
-		fmt.Println("worker", id, "result", j)
+		id += 1
+		fmt.Println(id)
 		results <- j
 
 	}
@@ -16,8 +17,8 @@ func main() {
 	jobs := make(chan int, 1000)
 	results := make(chan int, 1000)
 
-	for w := 1; w <= 500; w++ {
-		go worker(w, jobs, results)
+	for w := 1; w <= 10; w++ {
+		go worker(0, jobs, results)
 	}
 
 	for j := 1; j <= 1000; j++ {
